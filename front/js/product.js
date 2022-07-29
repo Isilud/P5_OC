d5e65883ac69a605a81d9e4d6952462be245afe1
Isilud <jolan.houot@gmail.com>
2022-07-29 11:51:31 +0200

    Fixed some JS. Can now add products to basket

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,30 @@
-window.onload = async function () {
-  productID = new URL(window.location.href).searchParams.get("id");
-  item = await fetchItem(productID);
-  showItem(item);
-};
+// Main
+localStorage.clear();
+productID = new URL(window.location.href).searchParams.get("id");
+fetchItem(productID);
 
-async function fetchItem(id) {
-  return await fetch("http://localhost:3000/api/products/" + id)
+document.getElementById(`addToCart`).addEventListener(`click`, function () {
+  let color = document.getElementById(`colors`).value;
+  let quantity = document.getElementById(`quantity`).value;
+  quantity = parseInt(quantity);
+  if (quantity == 0 || color == "") {
+    console.error("Please check your items informations.");
+  } else {
+    addToCart(productID, color, quantity);
+  }
+});
+
+// Functions Store
+
+function fetchItem(id) {
+  fetch("http://localhost:3000/api/products/" + id)
     .then((data) => {
       if (data.ok) {
         return data.json();
       }
     })
     .then((result) => {
-      return result;
+      showItem(result);
     })
     .catch((err) => {
       console.log(err);
@@ -40,4 +52,38 @@ function showItem(item) {
     newColor.value = color;
     colors.appendChild(newColor);
   }
+}
+
+function addToCart(productID, color, quantity) {
+  // On définit le produit en cours et on récupére le panier présent
+  newProduct = { id: productID, color: color, quantity: quantity };
+  let productList = fetchCart();
+
+  // On ajoute notre produit
+  let productIndex = productList.findIndex(
+    (product) =>
+      product.id == newProduct.id && product.color == newProduct.color
+  );
+  if (productIndex == -1) {
+    productList.push(newProduct);
+  } else {
+    productList[productIndex].quantity += newProduct.quantity;
+  }
+
+  // On met à jour notre panier dans le storage.
+  setCart(productList);
+}
+
+function fetchCart() {
+  if (localStorage.getItem("cart") == null) {
+    console.log("List is empty");
+    let emptyList = JSON.stringify({ productList: [] });
+    localStorage.setItem("cart", emptyList);
+  }
+  return JSON.parse(localStorage.getItem("cart"))["productList"];
+}
+
+function setCart(productList) {
+  cart = { productList: productList };
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,34 +1,35 @@
-window.onload = async function () {
-  items = await fetchItemList();
-  for (item of items) {
-    show(item);
-  }
-};
+// Main
 
-async function fetchItemList() {
-  return await fetch("http://localhost:3000/api/products")
+fetchItemList();
+
+// Functions Store
+
+function fetchItemList() {
+  fetch("http://localhost:3000/api/products")
     .then((data) => {
       if (data.ok) {
         return data.json();
       }
     })
-    .then((result) => {
-      return result;
+    .then((items) => {
+      show(items);
     })
     .catch((err) => {
       console.log(err);
       return nil;
     });
-}
+};
 
-function show(item) {
-  const newItem = document.createElement("a");
-  newItem.href = `./product.html?id=${item._id}`;
-  newItem.innerHTML = `<article>
+function show(items) {
+  for (item of items) {
+    const newItem = document.createElement("a");
+    newItem.href = `./product.html?id=${item._id}`;
+    newItem.innerHTML = `<article>
     <img src="${item.imageUrl}" alt="${item.altTxt}">
     <h3 class="productName">${item.name}</h3>
     <p class="productDescription">${item.description}</p>
   </article>`;
-  let element = document.getElementById("items");
-  element.appendChild(newItem);
+    let element = document.getElementById("items");
+    element.appendChild(newItem);
+  }
 }
